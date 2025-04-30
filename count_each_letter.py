514b1b3dddd5b6ce5c6caa069f57a802bbc934db
Painkiller995 <fahdakramdaher@gmail.com>
2025-04-30 22:33:06 +0200

    add check path

--- count_each_letter.py
+++ count_each_letter.py
@@ -1,6 +1,12 @@
+import os
 from collections import Counter
 
-with open("z_lorem_ipsum.txt", encoding="utf-8") as file:
+FILE_PATH = "z_lorem_ipsum.txt"
+
+if not os.path.exists(FILE_PATH):
+    raise FileNotFoundError("The file 'z_lorem_ipsum.txt' does not exist.")
+
+with open(FILE_PATH, encoding="utf-8") as file:
     letter_counts = Counter(char for line in file for char in line)
 
 for char, count in sorted(letter_counts.items()):
