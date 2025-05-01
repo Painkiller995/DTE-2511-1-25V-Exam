deb9247b68e72b0a1d8f1c67339948f460715223
Painkiller995 <fahdakramdaher@gmail.com>
2025-05-01 08:44:43 +0200

    add iterator example

---
+++ custom_iterator_example.py
@@ -0,0 +1,35 @@
+# Filename: custom_iterator_example.py
+
+
+class CountUpTo:
+    def __init__(self, max):
+        self.max = max
+        self.current = 1
+
+    def __iter__(self):
+        return self  # returns the iterator object itself
+
+    def __next__(self):
+        if self.current <= self.max:
+            num = self.current
+            self.current += 1
+            return num
+        else:
+            raise StopIteration
+
+
+def main():
+    print("Manual iteration using iter() and next():")
+    counter = CountUpTo(3)
+    iterator = iter(counter)  # get the iterator object
+
+    while True:
+        try:
+            value = next(iterator)  # manually get the next value
+            print(value)
+        except StopIteration:
+            break  # stop when iteration is complete
+
+
+if __name__ == "__main__":
+    main()
