150d9e596676d5cdba7578dbcaa9c495b0e0802d
Painkiller995 <fahdakramdaher@gmail.com>
2025-05-02 09:55:34 +0200

    fix func name

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -1,4 +1,4 @@
-def greates_common_divisor(a, b):
+def greatest_common_divisor(a, b):
     gcd = k = 1
     while k <= a and k <= b:
         if a % k == 0 and b % k == 0:
@@ -11,7 +11,7 @@ def main():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
 
-    print(f"The greatest common divisor of {a} and {b} is {greates_common_divisor(a, b)}")
+    print(f"The greatest common divisor of {a} and {b} is {greatest_common_divisor(a, b)}")
 
 
 if __name__ == "__main__":
