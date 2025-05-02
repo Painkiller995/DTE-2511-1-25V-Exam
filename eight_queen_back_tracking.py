5fd8fe9a16da85fdc579c395f70187768efbfc89
Painkiller995 <fahdakramdaher@gmail.com>
2025-05-03 00:40:02 +0200

    add eight queen example

---
+++ eight_queen_back_tracking.py
@@ -0,0 +1,33 @@
+SIZE = 4
+QUEENS = [-1] * SIZE
+
+
+def is_valid(desired_row, desired_column):
+    for row_index in range(desired_row):  # only check previous rows
+        column = QUEENS[row_index]
+
+        # Same column
+        if column == desired_column:
+            return False
+
+        # Same diagonal
+        if abs(desired_row - row_index) == abs(desired_column - column):
+            return False
+
+    return True
+
+
+def solve(row=0):
+    # Stop recursion if all queens are placed
+    if row == SIZE:
+        print("Solution:", QUEENS)
+        return  # or return True if only one solution is needed
+
+    for col in range(SIZE):
+        if is_valid(row, col):
+            QUEENS[row] = col
+            solve(row + 1)
+            QUEENS[row] = -1  # backtrack
+
+
+solve()
