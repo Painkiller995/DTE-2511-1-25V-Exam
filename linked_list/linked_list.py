95c75369f24e6e558a0d1378d9b6a7fbe727c9bf
Painkiller995 <fahdakramdaher@gmail.com>
2025-05-04 22:54:05 +0200

    add size func

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -5,9 +5,11 @@ class LinkedList:
     def __init__(self):
         self._head = None
         self._tail = None
+        self._count = 0
 
     def add_first(self, value):
         new_node = Node(value)
+        self._count += 1
         if self.is_empty():
             self._head = self._tail = new_node
             return
@@ -17,6 +19,7 @@ class LinkedList:
 
     def add_last(self, value):
         new_node = Node(value)
+        self._count += 1
         if self.is_empty():
             self._head = self._tail = new_node
             return
@@ -26,6 +29,7 @@ class LinkedList:
     def remove_first(self):
         if self.is_empty():
             return None
+        self._count -= 1
         current_head_value = self._head.value
         self._head: Node = self._head.next
         self._head.prev = None
@@ -34,6 +38,7 @@ class LinkedList:
     def remove_last(self):
         if self.is_empty():
             return None
+        self._count -= 1
         current_tail_value = self._tail.value
         self._tail: Node = self._tail.prev
         self._tail.next = None
@@ -48,8 +53,12 @@ class LinkedList:
     def is_empty(self):
         return not self.get_first()
 
+    def size(self):
+        return self._count
+
     def clear(self):
         self._head = self._tail = None
+        self._count = 0
 
     def __iter__(self):
         current_node = self._head
@@ -63,8 +72,9 @@ def main():
     linked_list.add_last(8)
     linked_list.add_last(9)
     linked_list.add_first(7)
-    linked_list.remove_first()
-    linked_list.remove_last()
+
+    print(linked_list.size())
+
     for node in linked_list:
         print(node.value)
 
